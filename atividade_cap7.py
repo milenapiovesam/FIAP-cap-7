4d7860dd29cffd983f0d6233e361e1e1c0f433ae
gui <guilherme.deck1227@gmail.com>
2024-03-19 00:06:02 -0300

    atividade 7

--- atividade_cap7.py
+++ atividade_cap7.py
@@ -12,11 +12,3 @@ elif (montante >= 20000):
     print (f"Com o valor R${montante:.2f}, você poderia ter comprado um carro popular usado.")
 else:
     print (f"Com o valor de R${montante:.2f}, você poderia ter dado entrada em um carro.")
-
-#Versão elaborada pelo Ben
-if montante < 20000:
-    print("Com o valor R${}, você poderia ter dado entrada em um carro".format(montante))
-elif montante >= 20000 and montante <= 50000: #Ben, aqui troquei o "or" pelo "and" de acordo com o que conversamos.
-    print("Com o valor R${}, você poderia ter comprado um carro popular".format(montante))
-else: 
-    print("Com o valor R${}, você poderia ter comprado um carro zero".format(montante))
